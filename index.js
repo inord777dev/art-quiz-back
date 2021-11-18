09415aa3d4ed6fa59d3844d56b61c35e38218bd0
inord777dev <inord777dev@gmail.com>
2021-11-18 23:57:41 +0300

    init: new hello world

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + "/public"));
-app.use("/top10", function(request, response){
-     
-    response.send("<h1>TOP10</h1>");
-});
-app.listen(3000);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
