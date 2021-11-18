9567d274a18de8c69455f23280dfb95c6662b7cd
inord777dev <inord777dev@gmail.com>
2021-11-19 00:15:09 +0300

    feat: create  api template

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.get('/', function(request, response){
+  response.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get('/top10', function(request, response){
+  response.redirect('https://data.heroku.com/dataclips/jwlflqszgedmzgpymdmhivgaoifj.json');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
